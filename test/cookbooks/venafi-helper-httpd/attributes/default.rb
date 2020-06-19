ab158928a0ddf9011015e59799c88fc143e6b32e
skylerto <skylerclayne@gmail.com>
2020-06-19 17:57:20 -0400

    switching to vcert cli
    Signed-off-by: skylerto <skylerclayne@gmail.com>

---
+++ test/cookbooks/venafi-helper-httpd/attributes/default.rb
@@ -0,0 +1,8 @@
+default['venafi-helper-httpd']['url'] = ''
+default['venafi-helper-httpd']['username'] = ''
+default['venafi-helper-httpd']['password'] = ''
+default['venafi-helper-httpd']['zone'] = ''
+default['venafi-helper-httpd']['common_name'] = ''
+default['venafi-helper-httpd']['location'] = ''
+default['venafi-helper-httpd']['device_name'] = ''
+default['venafi-helper-httpd']['apikey'] = nil

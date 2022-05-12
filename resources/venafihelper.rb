5eff277e8b4b31b4c25eef565546008626ea0b1d
Gregory Schofield <greg.c.schofield@gmail.com>
2022-05-12 15:52:52 -0400

    Add properties for selecting a version of vcert to install
    Signed-off-by: Gregory Schofield <greg.c.schofield@gmail.com>

--- resources/venafihelper.rb
+++ resources/venafihelper.rb
@@ -1,4 +1,6 @@
 resource_name :venafihelper
+provides :venafihelper
+unified_mode true
 
 property :common_name, String, name_property: true
 property :tpp_url, String
@@ -7,13 +9,15 @@ property :tpp_password, String
 property :tpp_username, String
 property :zone, String
 property :location, String
-property :device_name, String, default: node['fqdn']
+property :device_name, String, default: lazy { node['fqdn'] }
 property :app_name, String
 property :apikey, String
 property :id_path, String
 property :app_info, String
 property :tls_address, String
 property :renew_threshold, Integer
+property :vcert_version, String, default: 'latest'
+property :vcert_download_url, String
 
 action :run do
   app_name = new_resource.app_name
@@ -32,16 +36,36 @@ action :run do
   key_path = "#{location}/#{key_file}"
   chain_path = "#{location}/#{chain_file}"
   id_path = "#{location}/#{id_file}"
-  app_info = "Indellient-ChefInfra-Helper"
+  app_info = 'Indellient-ChefInfra-Helper'
 
   if !instance.nil? && !new_resource.tls_address.nil? && !new_resource.apikey.nil?
     ::Chef::Application.fatal!('Device Registration not supported on Venafi Cloud')
   end
 
-  remote_file venafi_install_path do
-    source venafi_download_url
+  vcert_version = new_resource.vcert_version
+  vcert_platform =  'linux'
+  vcert_platform += '86' unless node['kernel']['machine'].eql? 'x86_64'
+
+  vcert_download_url = new_resource.vcert_download_url || venafi_download_url(vcert_version, vcert_platform)
+
+  remote_file "#{Chef::Config['file_cache_path']}/#{::File.basename(vcert_download_url)}" do
+    source vcert_download_url
     mode '0755'
     action :create
+    notifies :run, 'execute[extract]', :immediately
+  end
+
+  package 'unzip'
+
+  execute 'extract' do
+    command "unzip -o #{Chef::Config['file_cache_path']}/#{::File.basename(vcert_download_url)} -d #{Chef::Config['file_cache_path']}"
+    action :nothing
+    notifies :run, 'execute[copy]', :immediately
+  end
+
+  execute 'copy' do
+    command "cp #{Chef::Config['file_cache_path']}/vcert /usr/local/bin"
+    action :nothing
   end
 
   directory location
@@ -63,7 +87,7 @@ action :run do
       app_info: app_info,
       tls_address: new_resource.tls_address
     )
-    sensitive true
+    sensitive false
     not_if { ::File.exist?(cert_path) && ::File.exist?(key_path) && ::File.exist?(chain_path) }
   end
 
@@ -81,7 +105,7 @@ action :run do
       token: new_resource.token,
       tpp_url: new_resource.tpp_url
     )
-    sensitive true
+    sensitive false
     only_if { should_renew?(cert_path: cert_path, renew_threshold: new_resource.renew_threshold) }
   end
 end
